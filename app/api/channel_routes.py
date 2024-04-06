dd428faef17c4f6f1096068dccf3c858faae0254
Alex <ajtravis2@gmail.com>
2024-04-06 16:54:37 -0400

    added channel_routes file

---
+++ app/api/channel_routes.py
@@ -0,0 +1,4 @@
+from app.models import Channel
+from flask import Blueprint
+
+server_routes = Blueprint('channels', __name__)
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .db import db
 from .user import User
 from .server import Server
 from .db import environment, SCHEMA
+from .channel import Channel
